3f94dcdbeb20fd7abe433acb9ce4bc21d2e7d3af
rodrigopdo <rodrigo.prudente82@gmail.com>
2024-03-23 20:44:31 -0300

    First commit

---
+++ src/main/java/com/rpo/mimico/MimicoApplication.java
@@ -0,0 +1,13 @@
+package com.rpo.mimico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MimicoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rpo/mimico/MimicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpo.mimico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MimicoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
