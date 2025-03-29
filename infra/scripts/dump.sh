a429f512a35af913d8dc1136ccab7092b6ec56da
rodrigopdo <rodrigo.prudente82@gmail.com>
2025-03-29 08:09:01 -0300

    Fix: scripts in dump.sh and restore.sh.

--- infra/scripts/dump.sh
+++ infra/scripts/dump.sh
@@ -2,6 +2,6 @@
 
 echo "Starting database backup..."
 
-docker exex -t mimico_postgres pg_dump -U mimico mimico_db > ./db/backup.sql
+docker exec -t mimico_postgres pg_dump -U mimico mimico_db > ./db/backup.sql
 
-echo "Database backup has finished in: mimico-infra/db/backup.sql"
+echo "Database backup has finished in: infra/db/backup.sql"
--- infra/scripts/restore.sh
+++ infra/scripts/restore.sh
@@ -4,4 +4,4 @@ echo "Starting database restoring..."
 
 cat ./db/backup.sql | docker exec -i mimico_postgres psql -U mimico -d mimico_db
 
-echo "✅ Database restored successfully!"
+echo "Database restored successfully!"
