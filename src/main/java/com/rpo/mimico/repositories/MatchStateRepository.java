52561da78b7e4df9cb463f88af2996cd0ee8d779
Rodrigo Prudente <rodrigo.prudente82@gmail.com>
2025-10-15 11:31:09 -0300

    feat(MatchState) add findExpiredRounds method in MatchStateRepository

--- src/main/java/com/rpo/mimico/repositories/MatchStateRepository.java
+++ src/main/java/com/rpo/mimico/repositories/MatchStateRepository.java
@@ -2,8 +2,12 @@ package com.rpo.mimico.repositories;
 
 import com.rpo.mimico.entities.MatchStateEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,4 +15,12 @@ import java.util.UUID;
 public interface MatchStateRepository extends JpaRepository<MatchStateEntity, UUID> {
 
     Optional<MatchStateEntity> findByMatchId(UUID matchId);
+
+    @Query("SELECT ms FROM MatchStateEntity ms " +
+            "WHERE ms.roundExpiresAt IS NOT NULL " +
+            "AND ms.roundExpiresAt < : now " +
+            "AND ms.isPaused = false")
+    List<MatchStateEntity> findExpiredRounds(@Param("now")LocalDateTime now);
+
+
 }
