9421f4d7c6283c1664e37e007841f91cb5d5460e
Rodrigo Prudente <rodrigo.prudente82@gmail.com>
2025-10-21 09:53:35 -0300

    fix: in RegisterRequestDTO change range 5 to 20 @Size in nickname. Also change the pattern message of this field

--- src/main/java/com/rpo/mimico/dtos/RegisterRequestDTO.java
+++ src/main/java/com/rpo/mimico/dtos/RegisterRequestDTO.java
@@ -20,10 +20,10 @@ public record RegisterRequestDTO(
         String password,
 
         @NotBlank(message = "Nickname is required")
-        @Size(min = 3, max = 50, message = "Nickname must be between 3 and 50 characters")
+        @Size(min = 5, max = 20, message = "Nickname must be between 5 and 20 characters")
         @Pattern(
                 regexp = "^[a-zA-Z0-9_-]+$",
-                message = "Nickname can only contain letters, numbers, hyphens and underscores"
+                message ="Nickname must be a single word containing only letters, numbers, hyphens, or underscores"
         )
         String nickname
 ) {}
